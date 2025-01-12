81f077b82d94f7ca0f23f9aa147622f75465dfae
Duc Trung <trung.lyduc18@gmail.com>
2025-01-12 10:24:43 +0700

    CHECKPOINT: restore ssi_data_crawler

---
+++ stock_price_predictor/ssi_data_crawler/config.py
@@ -0,0 +1,8 @@
+import os
+
+auth_type = "Bearer"
+consumerID = os.getenv("consumerID")
+consumerSecret = os.getenv("consumerSecret")
+
+url = "https://fc-data.ssi.com.vn/"
+stream_url = "https://fc-datahub.ssi.com.vn"
