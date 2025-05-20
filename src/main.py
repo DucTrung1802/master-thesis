678d2eb2fdec68c5d53466327d6041c409822fdd
DucTrung1802 <trung.lyduc18@gmail.com>
2025-05-20 22:13:19 +0700

    ADD: minor fix

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@ from web_scraper.web_scraper import WebScraper
 
 def main():
     my_logger = Logger(file_name=LOG_FILE_BASE)
-    my_logger.log_info("START")
+    my_logger.log_info("\nSTART")
     my_web_scraper = WebScraper(logger=my_logger, power=100)
     my_web_scraper.start_scraping()
-
+    
 
 if __name__ == "__main__":
     main()
