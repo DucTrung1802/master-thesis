70cb571b14df986a4e36617ed0f32678588e24ce
DucTrung1802 <trung.lyduc18@gmail.com>
2025-06-22 00:53:42 +0700

    UPDATE: add visualization notebook

--- src/visualizer/visualizer.py
+++ src/visualizer/visualizer.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 import os
+import plotly.graph_objects as go
+import pandas as pd
 
 from logger.logger import Logger
 from tabular_database_driver.postgre_sql_driver import PostgreSQLDriver
@@ -32,5 +34,41 @@ class Visualizer:
 
         self._database_driver.connect(connection_model)
 
-    def visualize(self) -> None:
-        pass
+    def test_data(self) -> pd.DataFrame:
+        return self._database_driver.select(
+            schema_name="macroeconomics",
+            table_name="gdp",
+            order_by=["year", "quarter"],
+        )
+
+    def test_visualize(self) -> None:
+        df = self.test_data()
+        df["gdp_true_growth_acc"] = df["gdp_true_growth_acc"].fillna(0)
+        df["period"] = df["year"].astype(str) + "-Q" + df["quarter"].astype(str)
+        fig = go.Figure()
+
+        fig.add_trace(
+            go.Scatter(
+                x=df["period"],
+                y=df["gdp_true_growth_acc"],
+                mode="lines+markers",
+                name="GDP Growth",
+            )
+        )
+
+        fig.update_layout(
+            title="GDP True Growth Acc by Quarter",
+            xaxis_title="Year-Quarter",
+            yaxis_title="GDP Growth (%)",
+            xaxis=dict(
+                tickangle=30,  # less tilt
+                tickmode="auto",
+                nticks=30,  # reduce number of ticks shown
+                tickfont=dict(size=10),
+            ),
+            template="plotly_white",
+            height=500,
+            width=1000,
+        )
+
+        fig.show()
