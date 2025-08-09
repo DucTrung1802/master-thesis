72e935c42fb3dbad6f0b9c85ce3ae4d1d77fbe9d
DucTrung1802 <trung.lyduc18@gmail.com>
2025-08-10 01:02:51 +0700

    HOTFIX: _scrape_data_macroeconomics_ipi_vietstock()

--- src/web_scraper/web_scraper.py
+++ src/web_scraper/web_scraper.py
@@ -450,16 +450,13 @@ class WebScraper:
             WebDriverWait(web_driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, table_title_xpath))
             )
+            time.sleep(3)
 
             bs4_parser = self._update_bs4_parser(web_driver)
 
             headers, rows = self._extract_table_by_id(
                 bs4_parser=bs4_parser, id="tbl-macro-data"
             )
-
-            bs4_parser = self._update_bs4_parser(web_driver)
-
-            headers, rows = self._extract_table_by_id(bs4_parser, "tbl-macro-data")
 
             # Write to CSV
             with open(file_path, "w", newline="", encoding="utf-8") as f:
