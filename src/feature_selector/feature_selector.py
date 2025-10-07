92d39976aaac698459b0d57a7f9a7c06b7f9374a
DucTrung1802 <trung.lyduc18@gmail.com>
2025-10-07 08:00:38 +0700

    UPDATE: complete xgb_feature_selector

--- src/feature_selector/feature_selector.py
+++ src/feature_selector/feature_selector.py
@@ -2,6 +2,10 @@ from typing import List
 from dotenv import load_dotenv
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+import xgboost as xgb
 
 from logger.logger import Logger
 from dtos.tabular_database_driver_dtos.tabular_database_driver_dtos import (
@@ -21,8 +25,9 @@ load_dotenv()
 
 
 class FeatureSelector:
-    def __init__(self, logger: Logger, feature_selector_type: FeatureSelectorType = FeatureSelectorType):
+    def __init__(self, logger: Logger, feature_selector_type: FeatureSelectorType):
         self._logger = logger
+        self.feature_selector_type = feature_selector_type
         self._database_driver = PostgreSQLDriver(logger=logger)
         self.connect_to_database()
 
@@ -57,3 +62,65 @@ class FeatureSelector:
             order_by=order_by,
             limit=limit,
         )
+
+    def _fit_xgb_regressor(
+        self, dataframe: pd.DataFrame, feature_columns: List[str], target_column: str
+    ) -> None:
+        self._xgb_regressor = xgb.XGBRegressor(
+            base_score=0.5,
+            booster="gbtree",
+            n_estimators=1000,
+            early_stopping_rounds=50,
+            objective="reg:linear",
+            max_depth=3,
+            learning_rate=0.01,
+        )
+        self._xgb_regressor.fit(
+            dataframe[feature_columns],
+            dataframe[target_column],
+            eval_set=[(dataframe[feature_columns], dataframe[target_column])],
+            verbose=100,
+        )
+
+    def fit(self, dataframe: pd.DataFrame, target_column: str) -> None:
+        if not self.feature_selector_type:
+            raise ValueError("Feature selector type not set or not recognized.")
+
+        if target_column not in dataframe.columns:
+            raise ValueError(f"Target column '{target_column}' not found in dataframe.")
+
+        feature_columns = dataframe.columns.tolist()
+        feature_columns.remove(target_column)
+
+        if not feature_columns:
+            raise ValueError("No feature columns available for training.")
+
+        match self.feature_selector_type:
+            case FeatureSelectorType.XGB_REGRESSOR:
+                self._fit_xgb_regressor(dataframe, feature_columns, target_column)
+
+    def get_feature_importances(self) -> pd.Series:
+        if not self.feature_selector_type:
+            raise ValueError("Feature selector type not set or not recognized.")
+
+        match self.feature_selector_type:
+            case FeatureSelectorType.XGB_REGRESSOR:
+                if not hasattr(self, "_xgb_regressor"):
+                    raise ValueError("Model has not been fitted yet.")
+                return pd.Series(
+                    self._xgb_regressor.feature_importances_,
+                    index=self._xgb_regressor.get_booster().feature_names,
+                ).sort_values(ascending=False)
+
+    def plot_feature_importances(self, top_n: int = 20) -> None:
+        feature_importances = self.get_feature_importances().head(top_n)
+
+        plt.figure(figsize=(14, 8))
+        feature_importances.sort_values(ascending=True).plot(kind="barh")
+        plt.title(
+            f"Using '{self.feature_selector_type.value}' - Top Feature Importances"
+        )
+        plt.xlabel("Importance Score")
+        plt.ylabel("Features")
+        plt.tight_layout()
+        plt.show()
