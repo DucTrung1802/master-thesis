cdaa07636f8c7c024538d61adf21b53146f2841b
DucTrung1802 <trung.lyduc18@gmail.com>
2026-04-18 22:58:24 +0700

    UPDATE: WebScraper - adjust _scrape_data_stock_market_vn_index_price to scrape by page

--- src/web_scraper/web_scraper.py
+++ src/web_scraper/web_scraper.py
@@ -3094,18 +3094,86 @@ class WebScraper:
 
             # 6. Navigate to URL
             web_driver, bs4_parser = self._navigate_to_url(web_driver, source_info.url)
-            time.sleep(SCRAPER_BASE_WAIT_TIME)
+
+            date_expect_change_xpath = '//*[@id="render-table-owner"]/tr[1]/td[2]'
+            WebDriverWait(web_driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, date_expect_change_xpath))
+            )
 
             # 7. Logic for scraping
-            xpath = '//*[@id="tabletoExcel"]/img'
-            self._click_element(web_driver, xpath)
+            from_date_xpath = '//*[@id="date-from"]'
+            self._input_text(
+                web_driver,
+                from_date_xpath,
+                f"{SCRAPER_START_DATE.strftime('%d/%m/%Y')}",
+            )
 
-            download_file_name = "LichSuGia_VNINDEX__.xlsx"
-            download_file_path = os.path.join(DOWNLOAD_FOLDER_PATH, download_file_name)
+            find_button_xpath = '//*[@id="owner-find"]'
+            self._click_element(web_driver, find_button_xpath)
 
-            wait_for_file(download_file_path)
-            move_file(path_a=download_file_path, path_b=file_path)
-            convert_xlsx_to_csv(file_path)
+            total_page_xpath = '//*[@id="wraper-content-paging"]/div[11]/p'
+            WebDriverWait(web_driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, total_page_xpath))
+            )
+            total_page = int(
+                self._find_first_valid_element_by_xpath(
+                    web_driver, xpaths=[total_page_xpath]
+                ).text
+            )
+
+            column_names = [
+                "code",
+                "date",
+                "close",
+                "adjust",
+                "change",
+                "matching_volume",
+                "matching_value",
+                "negotiate_volume",
+                "negotiate_value",
+                "open",
+                "high",
+                "low",
+            ]
+
+            all_data = []
+
+            next_page_xpath = '//*[@id="divStart"]/div/div[3]/div[3]'
+
+            for page in range(1, total_page + 1):
+                WebDriverWait(web_driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, date_expect_change_xpath))
+                )
+                bs4_parser = self._update_bs4_parser(web_driver)
+
+                headers, rows = self._extract_table_by_id(
+                    bs4_parser=bs4_parser,
+                    id="owner-contents-table",
+                )
+
+                # assuming each row is a list matching column order
+                all_data.extend(rows)
+
+                date_string_to_compare = self._find_first_valid_element_by_xpath(
+                    web_driver, xpaths=[date_expect_change_xpath]
+                ).text
+
+                self._click_element(web_driver, next_page_xpath)
+
+                changed = self._wait_until_text_not_equals(
+                    web_driver,
+                    date_expect_change_xpath,
+                    expected_text=date_string_to_compare,
+                )
+                if not changed:
+                    self._logger.log_error(
+                        f"Failed to navigate to next page. Stop at page {page}."
+                    )
+                    break
+
+            # Create DataFrame once
+            df = pd.DataFrame(all_data, columns=column_names)
+            df.to_csv(file_path, index=False)
 
         finally:
             web_driver.close()
