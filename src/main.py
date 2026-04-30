9b1bc5fac5983ad4decea7ad56fec7f79afeda1a
DucTrung1802 <trung.lyduc18@gmail.com>
2026-04-30 11:01:52 +0700

    ADD: SwitchHandler class

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from logger.logger import Logger
 from web_scraper.web_scraper import WebScraper
 from data_postprocessor.data_postprocessor import DataPostprocessor
 from data_preprocessor.data_preprocessor import DataPreprocessor
+from utils.switch_handler import SwitchHandler
 from utils.constants import LOG_FILE_BASE
 from utils.enums import *
 
@@ -10,13 +11,17 @@ def main():
     my_logger = Logger(file_name=LOG_FILE_BASE)
     my_logger.log_info("START")
 
-    my_web_scraper = WebScraper(logger=my_logger, power=100)
+    my_switch_handler = SwitchHandler(logger=my_logger)
+
+    my_web_scraper = WebScraper(
+        logger=my_logger, switch_handler=my_switch_handler, power=100
+    )
     my_web_scraper.start_scraping()
 
-    my_data_preprocessor = DataPreprocessor(logger=my_logger)
-    my_data_preprocessor.ingest_bronze_data()
-    my_data_preprocessor.ingest_silver_data()
-    my_data_preprocessor.ingest_gold_data()
+    # my_data_preprocessor = DataPreprocessor(logger=my_logger)
+    # my_data_preprocessor.ingest_bronze_data()
+    # my_data_preprocessor.ingest_silver_data()
+    # my_data_preprocessor.ingest_gold_data()
 
     # my_data_postprocessor = DataPostprocessor(logger=my_logger)
     # my_data_postprocessor.export_common_dataframe_to_db()
